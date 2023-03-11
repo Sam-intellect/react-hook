ecf8d49338e7e9abbf5a987322d27c74b9b61adf
Sam-intellect <alukoolakunlesamuel@gmail.com>
2023-03-11 14:30:44 +0100

    useState

--- src/components/UsingUseState.tsx
+++ src/components/UsingUseState.tsx
@@ -1,14 +1,28 @@
 import React, { useState } from 'react';
 
 const UsingUseState = () => {
+  const [count, setCount] = useState(4);
+
+  const incrementCount = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+  const decrementCount = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
   return (
     <>
       <div className="flex mx-auto items-center gap-5">
-        <button className="bg-black text-white text-3xl p-2 w-16 rounded-lg">
+        <button
+          className="bg-black text-white text-3xl p-2 w-16 rounded-lg"
+          onClick={incrementCount}
+        >
           +
         </button>
-        <h1 className="text-5xl">0</h1>
-        <button className="bg-black text-white text-3xl p-2 w-16 rounded-lg">
+        <h1 className="text-5xl">{count}</h1>
+        <button
+          className="bg-black text-white text-3xl p-2 w-16 rounded-lg"
+          onClick={decrementCount}
+        >
           -
         </button>
       </div>
