11188db767b7ea4fffc5997785ff6009dd687ab8
Sam-intellect <alukoolakunlesamuel@gmail.com>
2023-03-11 22:07:29 +0100

    initial

--- src/components/UsingUseState.tsx
+++ src/components/UsingUseState.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const UsingUseState = () => {
   const [count, setCount] = useState(4);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   const incrementCount = () => {
     setCount((prevCount) => prevCount + 1);
@@ -9,9 +10,23 @@ const UsingUseState = () => {
   const decrementCount = () => {
     setCount((prevCount) => prevCount - 1);
   };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {};
+  }, []);
   return (
     <>
-      <div className="flex mx-auto items-center gap-5">
+      <div className="flex mx-auto place-content-center items-center gap-5">
         <button
           className="bg-black text-white text-3xl p-2 w-16 rounded-lg"
           onClick={incrementCount}
@@ -26,6 +41,8 @@ const UsingUseState = () => {
           -
         </button>
       </div>
+      <button onClick={reset}>RESET</button>
+      <div>{windowWidth}</div>
     </>
   );
 };
